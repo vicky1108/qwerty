32e0e05b2100c3bd23bb8c6659cfc45866ca8a61
vicky1108 <kelegei.klava@gmail.com>
2016-10-21 14:07:07 +0300

    new commit

---
+++ src/javaapp/JavaApp.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapp;
+
+/**
+ *
+ * @author keleg
+ */
+public class JavaApp {
+
+    String name;
+
+    public JavaApp(String name) {
+        this.name = name;
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
